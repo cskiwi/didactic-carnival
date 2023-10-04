163d1dcd3f176d575e11e9ca7d73be392f059916
cskiwi <glenn.latomme@gmail.com>
2023-10-04 10:58:41 +0200

    ci: more testing

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { environment } from '../environments/environment';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
-const productionModules = environment.production
+const productionModules = process.env.NODE_ENV
   ? [
       ServeStaticModule.forRoot({
         rootPath: join(__dirname, '..', 'client'),
